ab5706034a5e403059c15b3127954988c21f7dff
franck cuny <franck@lumberjaph.net>
2010-02-28 10:25:04 +0100

    initial commit

---
+++ lib/Plack/Middleware/ETag.pm
@@ -0,0 +1,33 @@
+package Plack::Middleware::ETag;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::ETag -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::ETag;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::ETag is
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck.cuny@rtgi.frE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
